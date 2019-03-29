ee3299baac4cb89a2986bdb8d743cbcd8f2ad4e1
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-29 16:30:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,8 +128,9 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C", "Assembly"]
+          :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
 programmer_hash[:dennis_ritchie][:languages]
 end
